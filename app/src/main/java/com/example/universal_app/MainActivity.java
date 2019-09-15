d69d291f71a107b9960bac85ea7c546b8c0d35c1
piernesjose@gmail.com <36456181+prnsgithub@users.noreply.github.com>
2019-09-15 18:15:54 +0700

    re update

--- app/src/main/java/com/example/universal_app/MainActivity.java
+++ app/src/main/java/com/example/universal_app/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.universal_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+        Toolbar tolbar = findViewById(R.id.toolbarmenu);
+        setSupportActionBar(tolbar);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +40,40 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_setting) {
+            return true;
+        }
+        if (id == R.id.menu_exit) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Exit Application")
+                    .setMessage("Are you sure to exit ?")
+                    .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            System.exit(0);
+                        }
+                    })
+                    .setNegativeButton("no",null)
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
